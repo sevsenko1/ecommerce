68bdbefa2db7b9831e802622cb30622bc573f30d
TheSevko_ <sevket_4010@outlook.com>
2025-12-10 15:28:50 +0300

    Birkaç gereksiz dosyalar ve kullanılmayan eklentiler siliindi.

--- ecommerce/next.config.ts
+++ ecommerce/next.config.ts
@@ -2,22 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.dummyjson.com",
-      },
-      {
-        protocol: "https",
-        hostname: "via.placeholder.com",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
